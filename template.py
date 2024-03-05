33a21a7439b15dd21f1397fb03cf64b66e03b97a
himanko <himankoboruah@gmail.com>
2024-03-05 19:41:51 +0530

    data ingestion (emergency dataset) added

--- template.py
+++ template.py
@@ -17,6 +17,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
+    "artifact/data_ingestion/",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
